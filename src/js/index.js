ccd951582fb01472b5edd4aeb43fe440f5e3bd02
zdsu43 <63268999+zdsu43@users.noreply.github.com>
2021-06-28 14:52:50 -0500

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,1 @@
-//variables
-var x = 109829100929;
-var y = 109829100929;
 
-document.write("X = " + x + "<br>Y = " + y + "<br><br><b>+</b> = " + x + y + "<br><b>x</b> = " + x * y + "<br><b>-</b> = 0000000000000000000000000");
